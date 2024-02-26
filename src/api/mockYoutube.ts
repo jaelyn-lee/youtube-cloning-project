807656217e1818e5707712ccf75b9fe433ef8384
Jaelyn Lee <leejay1020@gmail.com>
2024-02-26 22:34:10 +1300

    commented out mock data to fix typescript err

--- src/api/mockYoutube.ts
+++ src/api/mockYoutube.ts
@@ -1,26 +1,26 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-export default class MockYoutube {
-  constructor() {}
+// export default class MockYoutube {
+//   constructor() {}
 
-  async search(id: string | undefined) {
-    return id ? this.#searchById(id) : this.#searchMostPopular()
-  }
+//   async search(id: string | undefined) {
+//     return id ? this.#searchById(id) : this.#searchMostPopular()
+//   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async #searchById(id) {
-    return axios
-      .get(`/data/leo-woodall.json`)
-      .then((res) => res.data.items)
-      .then((items) =>
-        items.map((item: { id: { videoId: unknown } }) => ({
-          ...item,
-          id: item.id.videoId,
-        }))
-      )
-  }
+//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//   async #searchById(id) {
+//     return axios
+//       .get(`/data/leo-woodall.json`)
+//       .then((res) => res.data.items)
+//       .then((items) =>
+//         items.map((item: { id: { videoId: unknown } }) => ({
+//           ...item,
+//           id: item.id.videoId,
+//         }))
+//       )
+//   }
 
-  async #searchMostPopular() {
-    return axios.get(`/data/most-popular.json`).then((res) => res.data.items)
-  }
-}
+//   async #searchMostPopular() {
+//     return axios.get(`/data/most-popular.json`).then((res) => res.data.items)
+//   }
+// }
